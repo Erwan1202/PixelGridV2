4f8bd1b0ed0eef294444d18dce0ede11deba2121
Erwan <erwan.a.marechal@gmail.com>
2025-11-13 21:05:19 +0100

    Feat(client): Create AuthForm component and update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css';
+import { useAuth } from '../context/AuthContext.jsx';
+import { AuthForm } from './components/AuthForm';
 
+// Main application component
 function App() {
+  const { user, loading, logout } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // Render the main application
   return (
     <div className="App">
-      <h1>Pixel Grid</h1>
+      <h1>Welcome to jurassic park</h1>
+      {user ? (
+        <div>
+          <p>Welcome, {user.username}!</p>
+          <button onClick={logout}>Logout</button>
+          {/* Le composant Grid (bientot inshallah) */}
+        </div>
+      ) : (
+        <AuthForm />
+      )}
     </div>
   );
 }
