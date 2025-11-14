b8c1159a20effedc55e0f2bf48149ea478258946
erwan <erwan.a.marechal@gmail.com>
2025-11-14 16:40:39 +0100

    refactor(routes): remove outdated comments and clarify base path for grid routes

--- server/src/routes/grid.routes.js
+++ server/src/routes/grid.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const gridController = require('../controllers/grid.controller');
 const checkJwt = require('../middlewares/checkJwt');
 
-// Mounted at `/api/grid` in routes/index.js
-// So base path here should be `/` (not `/grid`)
 router.get('/', gridController.getGrid);
 router.post('/pixel', checkJwt, gridController.placePixel);
 
